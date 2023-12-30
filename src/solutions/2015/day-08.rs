75ea5e77303684e508b78227b1a9bd206b79b607
Lars van Dartel <larsvandartel73@gmail.com>
2023-12-30 22:46:38 +0100

    2015-08

---
+++ src/solutions/2015/day-08.rs
@@ -0,0 +1,81 @@
+use aoc_rust::*;
+
+struct Day08 {
+    strings: Vec<String>,
+}
+
+impl Problem<usize, usize> for Day08 {
+    fn parse(input: &str) -> ParseResult<Self> {
+        Ok((
+            "",
+            Self {
+                strings: input.lines().map(|line| line.trim().to_string()).collect(),
+            },
+        ))
+    }
+
+    fn part1(self) -> Result<usize> {
+        Ok(self
+            .strings
+            .iter()
+            .map(|s| {
+                let mut chars = s.chars();
+                let mut count = 0;
+                while let Some(c) = chars.next() {
+                    if c == '\\' {
+                        match chars.next() {
+                            Some('x') => {
+                                chars.next();
+                                chars.next();
+                            }
+                            Some('\\') => {}
+                            Some('"') => {}
+                            None => break,
+                            _ => unreachable!(),
+                        }
+                    }
+                    count += 1;
+                }
+                s.len() - count + 2
+            })
+            .sum())
+    }
+
+    fn part2(self) -> Result<usize> {
+        Ok(self
+            .strings
+            .iter()
+            .map(|s| {
+                let mut count = 0;
+                for c in s.chars() {
+                    if c == '\\' || c == '"' {
+                        count += 1;
+                    }
+                }
+                count + 2
+            })
+            .sum())
+    }
+}
+
+aoc_main!(Day08);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = r#"""
+"abc"
+"aaa\"aaa"
+"\x27""#;
+
+    #[test]
+    fn test_part1() {
+        assert_task!(Day08, 1, EXAMPLE, 12);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_task!(Day08, 2, EXAMPLE, 19);
+    }
+}
