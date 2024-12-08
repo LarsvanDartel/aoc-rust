96a87fb60d37dbb669578a2c8c3a8cf6142c398e
Lars van Dartel <larsvandartel73@gmail.com>
2024-12-08 14:07:19 +0100

    added more input parsing

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -10,11 +10,12 @@ pub use vec::{Vec2, Vec3};
 
 pub use nom::{
     branch::alt,
-    bytes::complete::tag,
+    bytes::complete::{tag, take_till, take_until},
     character::complete::{
-        char, digit1, i32 as parse_i32, line_ending, one_of, space0, space1, u32 as parse_u32,
-        u64 as parse_u64,
+        alpha1, alphanumeric1, anychar, char, digit1, i32 as parse_i32, line_ending, multispace1,
+        one_of, space0, space1, u32 as parse_u32, u64 as parse_u64,
     },
+    combinator::verify,
     multi::{many1, separated_list1},
     sequence::{delimited, pair, preceded, separated_pair, terminated, tuple},
     Parser as _,
