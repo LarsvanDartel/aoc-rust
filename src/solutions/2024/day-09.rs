1d7af013df960ac295dfe68be00adb0c1e42c13e
Lars van Dartel <larsvandartel73@gmail.com>
2024-12-09 21:25:28 +0100

    improved 2024-09

--- src/solutions/2024/day-09.rs
+++ src/solutions/2024/day-09.rs
@@ -1,119 +1,131 @@
-use std::cmp::Ordering;
+use std::{
+    cmp::{Ordering, Reverse},
+    collections::BinaryHeap,
+};
 
 use aoc_rust::*;
 use common::*;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum File {
-    File(usize, usize),
-    Empty(usize),
+struct File {
+    desc: Option<usize>,
+    start: usize,
+    size: usize,
 }
 
-#[derive(Debug)]
-struct FileSystem(Vec<File>);
+struct FileSystem {
+    files: Vec<File>,
+    space: [BinaryHeap<Reverse<(usize, usize)>>; 10],
+}
 
 impl From<Vec<usize>> for FileSystem {
     fn from(filesystem: Vec<usize>) -> Self {
-        let filesystem = filesystem
-            .into_iter()
-            .enumerate()
-            .map(|(i, f)| {
-                if i % 2 == 1 {
-                    File::Empty(f)
-                } else {
-                    File::File(i / 2, f)
-                }
-            })
-            .collect::<Vec<_>>();
-        Self(filesystem)
+        let mut start = 0;
+        let mut files = Vec::new();
+        let mut space: [BinaryHeap<Reverse<(usize, usize)>>; 10] = Default::default();
+        for (i, size) in filesystem.into_iter().enumerate() {
+            if i % 2 == 0 {
+                files.push(File {
+                    desc: Some(i / 2),
+                    start,
+                    size,
+                });
+            } else {
+                files.push(File {
+                    desc: None,
+                    start,
+                    size,
+                });
+                space[size].push(Reverse((start, i)));
+            }
+            start += size;
+        }
+        if files.len() % 2 == 1 {
+            files.push(File {
+                desc: None,
+                start,
+                size: 0,
+            });
+        }
+
+        Self { files, space }
     }
 }
 
 impl FileSystem {
     fn compact(&mut self, preserve_contiguous: bool) {
-        let mut i = self.0.len() - 1;
-        let mut j = 0;
-        while i > j {
-            match (self.0[i], self.0[j]) {
-                (File::Empty(_), _) => i -= 1,
-                (_, File::File(_, _)) => {
-                    j += 1;
-                    if j >= i {
-                        j = 0;
-                        i -= 1;
-                    }
-                }
-                (File::File(desc, s1), File::Empty(s2)) => {
-                    if preserve_contiguous {
-                        if s1 > s2 {
-                            j += 1;
-                            if j >= i {
-                                j = 0;
-                                i -= 1;
-                            }
-                            continue;
-                        }
-                        self.0[j] = File::File(desc, s1);
-                        self.0[i] = File::Empty(s1);
-                        let rem = s2 - s1;
-                        if rem > 0 {
-                            self.0.insert(j + 1, File::Empty(rem));
-                        } else {
-                            i -= 1;
-                        }
-                        j = 0;
-                    } else {
-                        self.0[j] = File::File(desc, s1.min(s2));
-                        match s1.cmp(&s2) {
-                            Ordering::Greater => {
-                                self.0[i] = File::File(desc, s1 - s2);
-                                self.0.insert(i + 1, File::Empty(s2));
-                            }
-                            Ordering::Less => {
-                                self.0[i] = File::Empty(s1);
-                                self.0.insert(j + 1, File::Empty(s2 - s1));
-                            }
-                            Ordering::Equal => {
-                                self.0[i] = File::Empty(s1);
-                                i += 1;
-                            }
-                        }
-                        j -= 1;
-                    }
-                }
-            }
-        }
+        let mut l_idx = 1;
+        let mut r_idx = self.files.len() - 2;
+        while r_idx > 0 {
+            let File {
+                desc,
+                size: r_size,
+                start: r_start,
+            } = self.files[r_idx];
 
-        let mut i = self.0.len() - 1;
-        while i > 0 {
-            match (self.0[i], self.0[i - 1]) {
-                (File::Empty(s1), File::Empty(s2)) => {
-                    self.0[i - 1] = File::Empty(s1 + s2);
-                    self.0.remove(i);
+            if preserve_contiguous {
+                l_idx = self.space[r_size..10]
+                    .iter()
+                    .flat_map(|heap| heap.peek().cloned())
+                    .max() // Max here, because the elements are reversed
+                    .map(|Reverse((_start, idx))| idx)
+                    .unwrap_or(r_idx);
+            } else {
+                while l_idx <= r_idx && self.files[l_idx].size == 0 {
+                    l_idx += 2;
                 }
-                (File::File(d1, s1), File::File(d2, s2)) if d1 == d2 => {
-                    self.0[i - 1] = File::File(d1, s1 + s2);
-                    self.0.remove(i);
-                }
-                _ => (),
             }
-            i -= 1;
+            if l_idx >= r_idx {
+                r_idx -= 2;
+                continue;
+            }
+
+            let File {
+                start: l_start,
+                size: l_size,
+                ..
+            } = self.files[l_idx];
+
+            match l_size.cmp(&r_size) {
+                Ordering::Less => {
+                    self.files[l_idx].desc = desc;
+                    self.files[r_idx].size -= l_size;
+                    self.files[r_idx + 1].size += l_size;
+                    self.files[r_idx + 1].start -= l_size;
+                    l_idx += 2;
+                }
+                Ordering::Equal => {
+                    self.files[l_idx].start = r_start;
+                    self.files[r_idx].start = l_start;
+                    r_idx -= 2;
+                    l_idx += 2;
+                }
+                Ordering::Greater => {
+                    self.files[l_idx].size -= r_size;
+                    self.files[l_idx].start += r_size;
+                    self.files[r_idx].start = l_start;
+                    self.files[r_idx + 1].size += r_size;
+                    self.files[r_idx + 1].start -= r_size;
+                    r_idx -= 2;
+                    if preserve_contiguous {
+                        self.space[l_size - r_size].push(Reverse((l_start + r_size, l_idx)));
+                    }
+                }
+            }
+            if preserve_contiguous {
+                self.space[l_size].pop();
+            }
         }
     }
 
     fn checksum(&self) -> usize {
-        let mut checksum = 0;
-        let mut i = 0;
-        for file in &self.0 {
-            match file {
-                File::File(desc, size) => {
-                    checksum += desc * (i * size + size * (size - 1) / 2);
-                    i += size;
-                }
-                File::Empty(size) => i += size,
+        self.files.iter().fold(0, |checksum, f| {
+            let File { start, size, desc } = f;
+            if let Some(desc) = desc {
+                checksum + desc * (start * size + size * (size - 1) / 2)
+            } else {
+                checksum
             }
-        }
-        checksum
+        })
     }
 }
 
