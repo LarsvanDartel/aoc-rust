31d43bb00a50fc004f8ca067106c52e035826988
Lars van Dartel <larsvandartel73@gmail.com>
2024-12-11 09:48:23 +0100

    change string to u64

--- src/solutions/2024/day-11.rs
+++ src/solutions/2024/day-11.rs
@@ -2,27 +2,33 @@ use aoc_rust::*;
 use common::*;
 
 struct Day11 {
-    stones: HashMap<String, usize>,
+    stones: HashMap<u64, usize>,
 }
 
 impl Day11 {
     fn update(&mut self) {
         let mut new_map = HashMap::new();
         for (stone, count) in self.stones.clone() {
-            if stone == "0" || stone.is_empty() {
-                *new_map.entry("1".to_string()).or_default() += count;
+            if stone == 0 {
+                *new_map.entry(1).or_default() += count;
                 continue;
-            } else if stone.len() % 2 == 0 {
-                let mid = stone.len() / 2;
-                let (left, mut right) = stone.split_at(mid);
-                *new_map.entry(left.to_string()).or_default() += count;
-                right = right.trim_start_matches('0');
-                *new_map.entry(right.to_string()).or_default() += count;
+            }
+            let mut s = stone;
+            let mut l = 0;
+            let mut p = 1;
+            while s > 0 {
+                if l % 2 == 0 {
+                    p *= 10;
+                }
+                s /= 10;
+                l += 1;
+            }
+
+            if l % 2 == 0 {
+                *new_map.entry(stone / p).or_default() += count;
+                *new_map.entry(stone % p).or_default() += count;
             } else {
-                // Multiply by 2024
-                *new_map
-                    .entry((stone.parse::<usize>().unwrap() * 2024).to_string())
-                    .or_default() += count;
+                *new_map.entry(stone * 2024).or_default() += count;
             }
         }
         self.stones = new_map;
@@ -31,11 +37,11 @@ impl Day11 {
 
 impl Problem<usize, usize> for Day11 {
     fn parse(input: &str) -> ParseResult<Self> {
-        separated_list1(space1, digit1)
-            .map(|stones: Vec<&str>| {
+        separated_list1(space1, parse_u64)
+            .map(|stones| {
                 let mut map = HashMap::new();
                 for s in stones {
-                    *map.entry(s.to_string()).or_default() += 1;
+                    *map.entry(s).or_default() += 1;
                 }
                 Day11 { stones: map }
             })
@@ -69,10 +75,5 @@ mod tests {
     #[test]
     fn test_part1() {
         assert_task!(Day11, 1, EXAMPLE, 55312);
-    }
-
-    #[test]
-    fn test_part2() {
-        assert_task!(Day11, 2, EXAMPLE, ());
     }
 }
