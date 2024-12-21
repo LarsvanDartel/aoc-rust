7d4133ef170625b7631ec8b595f625157fb2c175
Lars van Dartel <larsvandartel73@gmail.com>
2024-12-21 15:10:50 +0100

    optimized

--- src/solutions/2024/day-21.rs
+++ src/solutions/2024/day-21.rs
@@ -1,122 +1,140 @@
-use std::cmp::Ordering;
+use std::iter;
 
 use aoc_rust::*;
 use common::*;
 
 struct Day21 {
+    keypad_num: KeyPad,
+    keypad_dir: KeyPad,
     codes: Vec<String>,
-    memo: HashMap<u64, usize>,
+}
+
+struct KeyPad {
+    paths: HashMap<(char, char), String>,
+}
+
+impl KeyPad {
+    const KEYPAD_NUM: [(Vec2<usize>, char); 11] = [
+        (Vec2::new(0, 0), '7'),
+        (Vec2::new(1, 0), '8'),
+        (Vec2::new(2, 0), '9'),
+        (Vec2::new(0, 1), '4'),
+        (Vec2::new(1, 1), '5'),
+        (Vec2::new(2, 1), '6'),
+        (Vec2::new(0, 2), '1'),
+        (Vec2::new(1, 2), '2'),
+        (Vec2::new(2, 2), '3'),
+        (Vec2::new(1, 3), '0'),
+        (Vec2::new(2, 3), 'A'),
+    ];
+
+    const KEYPAD_DIR: [(Vec2<usize>, char); 5] = [
+        (Vec2::new(1, 0), '^'),
+        (Vec2::new(2, 0), 'A'),
+        (Vec2::new(0, 1), '<'),
+        (Vec2::new(1, 1), 'v'),
+        (Vec2::new(2, 1), '>'),
+    ];
+
+    fn new<I>(iter: I) -> Self
+    where
+        I: Iterator<Item = (Vec2<usize>, char)> + Clone,
+    {
+        let map: HashMap<_, _> = iter.collect();
+        let mut paths = HashMap::new();
+        for &pos in map.keys() {
+            for &next in map.keys() {
+                let mut path = String::new();
+                let mut p = pos;
+                while p != next {
+                    if next.x < p.x && map.contains_key(&Vec2::new(next.x, p.y)) {
+                        path.extend(iter::repeat_n('<', p.x - next.x));
+                        p.x = next.x;
+                    }
+                    if next.y < p.y && map.contains_key(&Vec2::new(p.x, next.y)) {
+                        path.extend(iter::repeat_n('^', p.y - next.y));
+                        p.y = next.y;
+                    }
+                    if next.y > p.y && map.contains_key(&Vec2::new(p.x, next.y)) {
+                        path.extend(iter::repeat_n('v', next.y - p.y));
+                        p.y = next.y;
+                    }
+                    if next.x > p.x && map.contains_key(&Vec2::new(next.x, p.y)) {
+                        path.extend(iter::repeat_n('>', next.x - p.x));
+                        p.x = next.x;
+                    }
+                }
+                path.push('A');
+                paths.insert((map[&pos], map[&next]), path);
+            }
+        }
+        Self { paths }
+    }
+}
+
+fn hash(a: char, b: char, n_keypads: usize) -> u64 {
+    let mut hash = 0;
+    hash |= a as u64;
+    hash |= (b as u64) << 8;
+    hash |= (n_keypads as u64) << 16;
+    hash
 }
 
 impl Day21 {
-    const KEYPAD_NUMERIC: [[char; 3]; 4] = [
-        ['7', '8', '9'],
-        ['4', '5', '6'],
-        ['1', '2', '3'],
-        [' ', '0', 'A'],
-    ];
-    const KEYPAD_DIRECTION: [[char; 3]; 2] = [[' ', '^', 'A'], ['<', 'v', '>']];
-
-    fn hash(pos: Vec2<usize>, next: Vec2<usize>, n_keypads: usize) -> u64 {
-        let mut hash = 0;
-        hash |= pos.x as u64;
-        hash |= (pos.y as u64) << 8;
-        hash |= (next.x as u64) << 16;
-        hash |= (next.y as u64) << 24;
-        hash |= (n_keypads as u64) << 32;
-        hash
+    fn new(codes: Vec<String>) -> Self {
+        Self {
+            keypad_num: KeyPad::new(KeyPad::KEYPAD_NUM.into_iter()),
+            keypad_dir: KeyPad::new(KeyPad::KEYPAD_DIR.into_iter()),
+            codes,
+        }
     }
 
-    fn cost_step(
-        &mut self,
-        pos: Vec2<usize>,
-        next: Vec2<usize>,
-        keypads: &[&[[char; 3]]],
+    fn cost(
+        &self,
+        presses: &str,
+        n_keypads: usize,
+        keypad: &KeyPad,
+        cache: &mut HashMap<u64, usize>,
     ) -> usize {
-        let hash = Self::hash(pos, next, keypads.len());
-        if let Some(&cost) = self.memo.get(&hash) {
-            return cost;
-        }
-        let mut cost = usize::MAX;
-        let mut q = VecDeque::new();
-        q.push_back((pos, String::new()));
-        let keypad = keypads[0];
-        while let Some((mut p, presses)) = q.pop_front() {
-            if p == next {
-                cost = cost.min(self.cost(&(presses + "A"), &keypads[1..]));
-                continue;
-            }
-            if keypad[p.y][p.x] == ' ' {
-                continue;
-            }
-
-            match p.x.cmp(&next.x) {
-                Ordering::Less => {
-                    p.x += 1;
-                    q.push_back((p, presses.clone() + ">"));
-                    p.x -= 1;
-                },
-                Ordering::Greater => {
-                    p.x -= 1;
-                    q.push_back((p, presses.clone() + "<"));
-                    p.x += 1;
-                },
-                Ordering::Equal => {},
-            }
-            match p.y.cmp(&next.y) {
-                Ordering::Less => {
-                    p.y += 1;
-                    q.push_back((p, presses.clone() + "v"));
-                    p.y -= 1;
-                },
-                Ordering::Greater => {
-                    p.y -= 1;
-                    q.push_back((p, presses.clone() + "^"));
-                    p.y += 1;
-                },
-                Ordering::Equal => {},
-            }
-        }
-
-        self.memo.insert(hash, cost);
-        cost
-    }
-
-    fn cost(&mut self, presses: &str, keypads: &[&[[char; 3]]]) -> usize {
-        if keypads.is_empty() {
+        if n_keypads == 0 {
             return presses.len();
         }
-        let keypad = keypads[0];
-        let mut pos: Vec2<usize> = keypad
-            .iter()
-            .enumerate()
-            .find_map(|(i, row)| row.iter().position(|&c| c == 'A').map(|j| (j, i)))
-            .unwrap()
-            .into();
 
-        let mut cost = 0;
+        let mut pos = 'A';
+        let mut total_cost = 0;
 
         for key in presses.chars() {
-            let next_pos: Vec2<usize> = keypad
-                .iter()
-                .enumerate()
-                .find_map(|(i, row)| row.iter().position(|&ch| ch == key).map(|j| (j, i)))
-                .unwrap()
-                .into();
-            cost += self.cost_step(pos, next_pos, keypads);
-            pos = next_pos;
+            let hash = hash(pos, key, n_keypads);
+            let cost = if let Some(&cost) = cache.get(&hash) {
+                cost
+            } else {
+                let path = &keypad.paths[&(pos, key)];
+                let cost = self.cost(path, n_keypads - 1, &self.keypad_dir, cache);
+                cache.insert(hash, cost);
+                cost
+            };
+            total_cost += cost;
+            pos = key;
         }
 
-        cost
+        total_cost
     }
 
-    fn complexity(&mut self, keypads: &[&[[char; 3]]]) -> usize {
+    fn complexity(&mut self, n_keypads: usize) -> usize {
         let codes = self.codes.clone();
+        let mut cache = HashMap::new();
         codes
             .iter()
             .map(|code| {
-                let cost = self.cost(code, keypads);
-                cost * code.strip_suffix('A').unwrap().parse::<usize>().unwrap()
+                let cost = self.cost(code, n_keypads, &self.keypad_num, &mut cache);
+                let num = code.chars().fold(0, |acc, key| {
+                    if key.is_ascii_digit() {
+                        acc * 10 + key.to_digit(10).unwrap() as usize
+                    } else {
+                        acc
+                    }
+                });
+                cost * num
             })
             .sum()
     }
@@ -125,29 +143,16 @@ impl Day21 {
 impl Problem<usize, usize> for Day21 {
     fn parse(input: &mut &str) -> PResult<Self> {
         list(alphanumeric1.map(String::from), line_ending)
-            .map(|codes| Self {
-                codes,
-                memo: HashMap::new(),
-            })
+            .map(Self::new)
             .parse_next(input)
     }
 
     fn part1(mut self) -> Result<usize> {
-        let mut keypads: Vec<&[[char; 3]]> = Vec::new();
-        keypads.push(&Self::KEYPAD_NUMERIC);
-        for _ in 0..2 {
-            keypads.push(&Self::KEYPAD_DIRECTION);
-        }
-        Ok(self.complexity(&keypads))
+        Ok(self.complexity(3))
     }
 
     fn part2(mut self) -> Result<usize> {
-        let mut keypads: Vec<&[[char; 3]]> = Vec::new();
-        keypads.push(&Self::KEYPAD_NUMERIC);
-        for _ in 0..25 {
-            keypads.push(&Self::KEYPAD_DIRECTION);
-        }
-        Ok(self.complexity(&keypads))
+        Ok(self.complexity(26))
     }
 }
 
